03d723b8dc7449667b8e553b97735ab0de1fa430
Philip Lindberg <philip.c.lindberg@gmail.com>
2023-03-09 13:24:49 +0000

    Add expo blur

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { BlurView } from 'expo-blur'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{ uri: 'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=70' }}
+        resizeMode='cover'
+        style={[StyleSheet.absoluteFill]}
+      />
+      <BlurView intensity={100} style={styles.blurView}>
+        <Text style={styles.text}>Hello World!</Text>
+      </BlurView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  blurView: {
+    padding: 20,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+  },
+})
